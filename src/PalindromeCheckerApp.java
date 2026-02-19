37c4d19e664f3bd7256cc860dfab302901001ae3
VDhatri <dv6356@srmist.edu.in>
2026-02-19 09:44:12 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -8,11 +8,29 @@ class Palindrome{
         System.out.println("System initialized successfully");
     }
 
+    void checkStringPalindrome(String s){
+        int size = s.length();
+        boolean isPalindrome = true;
+        for(int i = 0 ; i < size ; i++){
+            if(s.charAt(i) != s.charAt(size - 1 - i)){
+                isPalindrome = false;
+                break;
+            }
+        }
+        if(isPalindrome){
+            System.out.println("The given string is a palindrome");
+        }else{
+            System.out.println("The given string is not a palindrome");
+        }
+    }
+
+
 }
 
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         Palindrome p = new Palindrome();
         p.start();
+        p.checkStringPalindrome("abccba");
     }
 }
