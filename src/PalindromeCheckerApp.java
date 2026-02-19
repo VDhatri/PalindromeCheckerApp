a545684eebb01e53a8edfe5b8575cc0cc1dfac81
VDhatri <dv6356@srmist.edu.in>
2026-02-19 08:55:26 +0530

    Base application set up

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Palindrome(){
+    void start(){
+        System.out.println("")
+    }
+}
+
+
+
+
+
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+
+    }
+}
